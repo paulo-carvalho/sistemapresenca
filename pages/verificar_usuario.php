9cf7a1da7d053774d55b4fd23149da2b90a6903f
paulo-carvalho <paulocarvalho@dcc.ufmg.br>
2016-02-06 12:04:23 -0200

    Teste autenticação

--- pages/verificar_usuario.php
+++ pages/verificar_usuario.php
@@ -4,20 +4,14 @@
 // require_once("seguranca.php");
 
 // Verifica se um formulário foi enviado
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  // Salva duas variáveis com o que foi digitado no formulário
-  // Detalhe: faz uma verificação com isset() pra saber se o campo foi preenchido
-  $usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : '';
-  $senha = (isset($_POST['senha'])) ? $_POST['senha'] : '';
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Salva duas variáveis com o que foi digitado no formulário
+    // Detalhe: faz uma verificação com isset() pra saber se o campo foi preenchido
+    $matricula = (isset($_POST['matricula'])) ? $_POST['matricula'] : '';
+    $senha = (isset($_POST['senha'])) ? $_POST['senha'] : '';
 
-  // Utiliza uma função criada no seguranca.php pra validar os dados digitados
-  if (validaUsuario($usuario, $senha) == true) {
+    var_dump ($matricula);
     // O usuário e a senha digitados foram validados, manda pra página interna
-    header("Location: index.php");
-  } else {
-    // O usuário e/ou a senha são inválidos, manda de volta pro form de login
-    // Para alterar o endereço da página de login, verifique o arquivo seguranca.php
-    expulsaVisitante();
-  }
-}
+    header("Location: pages/home.php");
+// }
 ?>
